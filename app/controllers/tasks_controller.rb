6ed793253e2f62a4b22fb91018fde9837c5f7492
Tech.Academy.291.5785 <techacademy.291.5785@gmail.com>
2021-06-16 08:04:49 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,22 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks
+    end
   end
 
   def show
-    @task= Task.find(params[:id])
+    @task= current_user.tasks.find(params[:id])
   end
 
   def new
-    @task= Task.new
+    @task= current_user.tasks.new
   end
 
   def create
-    @task= Task.new(task_params)
+    @task= current_user.tasks.new(task_params)
 
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
@@ -24,11 +28,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task= Task.find(params[:id])
+    @task= current_user.tasks.find(params[:id])
   end
 
   def update
-    @task= Task.find(params[:id])
+    @task= current_user.tasks.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -40,7 +44,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task= Task.find(params[:id])
+    @task= current_user.tasks.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Taskは正常に削除されました'
@@ -52,5 +56,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    unless @micropost
+      redirect_to root_url
+    end
   end
 end
